64c0ffab704544c744238cf4d3c04b91a33f3a88
Mostofa Mohiuddin <mostofa.mohiuddin@optimizely.com>
2024-02-13 02:23:51 +0600

    add route protection

--- frontend/src/pages/sign-up/index.tsx
+++ frontend/src/pages/sign-up/index.tsx
@@ -1,5 +1,5 @@
-import { Form, Input, Button } from 'antd';
-import { Link } from 'react-router-dom';
+import { Form, Input, Button, notification } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
 
 import type { ISignUpData } from '../../common/types';
 import { useAuthentication } from '../../hooks/authentication';
@@ -7,31 +7,45 @@ import { useAuthentication } from '../../hooks/authentication';
 export default function SignUp() {
   const { signUp, loading } = useAuthentication();
 
+  const [api, contextHolder] = notification.useNotification();
+
+  const navigate = useNavigate();
+
   const onFinish = async (data: ISignUpData) => {
-    await signUp(data);
+    const responseData = await signUp(data);
+    if (responseData) {
+      api['success']({
+        message: 'User Created',
+        description: 'User has been created successfully. Please sign in to continue.',
+      });
+      navigate('/sign-in');
+    }
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <Form name="basic" initialValues={{ remember: true }} onFinish={onFinish} style={{ width: 300 }}>
-        <div style={{ textAlign: 'center', marginBottom: '16px', fontWeight: 'bold', fontSize: '2rem' }}>Sign Up</div>
-        <Form.Item name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
-          <Input placeholder="Username" />
-        </Form.Item>
+    <>
+      {contextHolder}
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <Form name="basic" initialValues={{ remember: true }} onFinish={onFinish} style={{ width: 300 }}>
+          <div style={{ textAlign: 'center', marginBottom: '16px', fontWeight: 'bold', fontSize: '2rem' }}>Sign Up</div>
+          <Form.Item name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
+            <Input placeholder="Username" />
+          </Form.Item>
 
-        <Form.Item name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
-          <Input.Password placeholder="Password" />
-        </Form.Item>
+          <Form.Item name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+            <Input.Password placeholder="Password" />
+          </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" style={{ width: '100%' }} loading={loading}>
-            Sign Up
-          </Button>
-          <div style={{ textAlign: 'center', marginTop: '16px' }}>
-            Or <Link to="/sign-in">Sign In Now!</Link>
-          </div>
-        </Form.Item>
-      </Form>
-    </div>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ width: '100%' }} loading={loading}>
+              Sign Up
+            </Button>
+            <div style={{ textAlign: 'center', marginTop: '16px' }}>
+              Or <Link to="/sign-in">Sign In Now!</Link>
+            </div>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
   );
 }
